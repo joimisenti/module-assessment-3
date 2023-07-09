d6598fccebecfafc14fefb7c1a26e433a91da6e3
Joi Misenti <joipolloi1@gmail.com>
2023-07-09 18:52:44 -0400

    edited JavaScript

--- about.js
+++ about.js
@@ -5,10 +5,19 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Form successfully submitted!');
 }
-
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const catCompliment = evt => {
+	evt.preventDefault();
+
+	alert('Be hypnotized by the fuzzy belly.');
+}
+
+let catPic = document.querySelector('img');
+
+catPic.addEventListener("mouseover", catCompliment);
